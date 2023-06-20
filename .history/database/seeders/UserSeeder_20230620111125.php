ec41f8d22b13c52cd434e840b5ce6592e9b1fc43
Osadiaye Solomon <osadiayesolomon@gmail.com>
2023-06-20 12:10:53 -0700

    updates admin functionality

---
+++ .history/database/seeders/UserSeeder_20230620111125.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        // Create 10 user records
+        $faker = Faker::create();
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => bcrypt('password'),
+                // Add other fields as needed
+            ]);
+        }
+    }
+}
+
---
+++ .history/database/seeders/UserSeeder_20230620111437.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        // Create 10 user records
+        $faker = Faker::create();
+        for ($i = 0; $i < 100; $i++) {
+            $user = User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => bcrypt('password'),
+                // Add other fields as needed
+            ]);
+
+            $serialNumber = 'SN-' . $user->id;
+            $user->update(['serial_number' => $serialNumber]);
+        }
+    }
+}
+
+
